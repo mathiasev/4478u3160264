209efe4e5e3b9b9a63686790f85c7b03df54a85a
mathiasev <mathiasev@hotmail.com>
2017-03-15 22:16:22 +1100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@ window.addEventListener('scroll', scrollCheck);
 function scrollCheck() {
     if (window.pageYOffset > 100) {
  document.getElementById('nav').classList.add('fixHeader');
+        document.getElementById('scroll').classList.add('scoll');
     }
     else if (window.pageXOffset <100) {
         document.getElementById('nav').classList.remove('fixHeader');
+        document.getElementById('scroll').classList.remove('scroll');
     } 
 }
